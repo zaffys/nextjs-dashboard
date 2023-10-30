7f70c853d3321e6eddf36275b20c3280282c8e94
Zafar Siddiqi <zsiddiqi@sovree.lan>
2023-10-29 23:31:32 -0400

    completed chapter 13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,12 +1,17 @@
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import Form from '@/app/ui/invoices/edit-form'
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data'
+import { notFound } from 'next/navigation'
 
 export default async function Page({ params }: { params: { id: string } }) {
   
   const id = params.id
   const invoice = await fetchInvoiceById(id)
   const customers = await fetchCustomers()
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function RootLayout({
   children,
@@ -8,7 +9,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
